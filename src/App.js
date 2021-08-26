a5bfdf870c677e2438a9a7c9d64cb9b429ace7ac
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-26 15:15:21 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+// Import Pages
+import ComplaintForm from './Complaints/pages/ComplaintForm';
+import SearchComplaint from './Complaints/pages/SearchComplaint';
+import LoginAdmin from './Admin/pages/LoginAdmin';
+import TicketList from './Admin/pages/TicketList';
+import AdminPage from './Admin/pages/AdminPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AdminPage />
+      {/* <ComplaintForm /> */}
+      {/* <SearchComplaint /> */}
+      {/* <LoginAdmin /> */}
+      {/* <TicketList /> */}
     </div>
   );
 }
