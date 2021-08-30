1260066fed608e2dd5e526cecfaf759854586a80
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-31 02:27:41 +0300

    Edit margin & padding

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,8 +2,8 @@ import { forwardRef } from "react";
 
 export const Input = forwardRef((props, ref) => {
   return (
-    <div className={props.className ? props.className : "mb-3"}>
-      <input ref={ref} {...props} className="p-1" type={props.type} />
+    <div className={props.className ? props.className : "mb-1"}>
+      <input ref={ref} {...props} className="form-control p-2" type={props.type} />
       <br />
       {props.error && <p className="text-danger">{props.error.message}</p>}
     </div>
